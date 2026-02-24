d921c1d06c12931581a9e43a297f52191981893f
Matheus Costa <contato.matheusccp5@gmail.com>
2026-02-24 10:47:28 -0300

    feat: responsividade da agenda semanal

--- src/components/schedule/WeeklySchedule.tsx
+++ src/components/schedule/WeeklySchedule.tsx
@@ -32,7 +32,7 @@ export const WeeklySchedule: React.FC<WeeklyScheduleProps> = ({
   onScheduleChange,
 }) => {
   return (
-    <div className="space-y-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
       {DAYS_OF_WEEK.map((day) => {
         const scheduledWorkout = schedule[day.value];
         
